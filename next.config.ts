1a575866c1b9cdf591dc5d84ec0dd50038c1bc4d
prudentbird <prudentbird@gmail.com>
2026-01-21 13:24:31 +0100

    feat: update next.config and add env validation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.ts";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typedRoutes: true,
+  cacheComponents: true,
+  experimental: {
+    typedEnv: true,
+    browserDebugInfoInTerminal: true,
+    turbopackFileSystemCacheForDev: true,
+    turbopackFileSystemCacheForBuild: true,
+  },
 };
 
 export default nextConfig;
